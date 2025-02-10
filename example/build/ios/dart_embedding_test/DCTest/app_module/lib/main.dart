d2a210d080ca65963cb2fbd8c6aab414b6197b17
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-02-10 20:36:08 +0100

    update

---
+++ example/build/ios/dart_embedding_test/DCTest/app_module/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SizedBox(
+    child: Text(
+      "flutter view running",
+      style: TextStyle(color: Colors.white),
+    ),
+  ));
+}
