7269a0f18bd16d9922ee89730c4269aeaadf3a0e
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-02-10 17:04:02 +0100

    experimenting with integrating dart engine to ios

---
+++ example/build/ios/dart_embedding_test/DCTest/DCTest/DCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCTestApp.swift
+//  DCTest
+//
+//  Created by Tahiru Agbanwa on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
