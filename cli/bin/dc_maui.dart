1400a598eb63b4438b2ca51e4bda64cb1b1edeec
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-02-17 08:42:03 +0100

    backdated

--- cli/bin/dc_maui.dart
+++
@@ -1,5 +0,0 @@
-import '../lib/dc_maui.dart' as dc_maui;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dc_maui.calculate()}!');
-}
--- cli/test/dc_maui_test.dart
+++
@@ -1,8 +0,0 @@
-import '../lib/dc_maui.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
